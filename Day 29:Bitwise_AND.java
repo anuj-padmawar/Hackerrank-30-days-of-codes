d8991f0b878ef0f549af11438e94e827576ed054
Anuj Padmawar <35773688+anuj-padmawar@users.noreply.github.com>
2020-06-15 09:25:32 +0530

    Add files via upload

---
+++ Day 29:Bitwise_AND.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+        int T=sc.nextInt();
+        for (int i=0;i<T;i++) 
+        {
+            int N=sc.nextInt();
+            int K=sc.nextInt();
+
+            System.out.println(solve(N, K));
+        }
+
+        sc.close();
+    }
+
+    static int solve(int N,int K) 
+    {
+        int result=0;
+        for (int A=1;A<=N;A++) 
+        {
+            for (int B=A+1;B<=N;B++) 
+            {
+                int current=A & B;
+                if (current>result && current<K) 
+                {
+                    result=current;
+                }
+            }
+        }
+        return result;
+    }
+}
